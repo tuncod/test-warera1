14859fe6fc2777098e01bd743f9ca459bcc6d2bd
tuncod <mdha0308+tmpgh@gmail.com>
2026-04-05 22:19:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/test-warera1/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
