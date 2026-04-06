d49cd38e58ff147c7c1ec0f81bd7e4b6e3797013
tuncod <mdha0308+tmpgh@gmail.com>
2026-04-06 02:05:15 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,37 @@
 from fastapi import FastAPI
 from mangum import Mangum
+import httpx
 
 app = FastAPI()
+
+API_AUTH_TOKEN = "Bearer wae_ae8dc4516462513ce1ea18db612e1fa2b458409fa214985db9dc84dd407c3bc2"
+
+countries = {
+    "tn": "6813b6d546e731854c7ac84e",
+}
+
+headers = {
+    "Accept": "*/*",
+    "Content-Type": "application/json",
+    "Authorization": API_AUTH_TOKEN,
+}
 
 @app.get("/api/hello")
 def hello():
     return {"message": "hello from fastapi"}
+
+@app.get("/api/{country}")
+async def call_api(country: str):
+    if country not in countries:
+        return {"error": "Country not found"}
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f"https://api2.warera.io/trpc/country.getAllCountries",
+            headers=headers,
+            json={"countryId": countries[country]},
+        )
+    
+    return response.json()
 
 handler = Mangum(app)
