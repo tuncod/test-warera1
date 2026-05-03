4d6f70feb1ab429c4e7c3a08983b6921099b870b
Tuncode <mdha0308+tmpgh@gmail.com>
2026-05-03 21:36:17 +0000

    jdjd

---
+++ assets/vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+//  base: '/test-warera1/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three']
+        }
+      }
+    }
+  },
+  plugins: [vue(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 8000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 8000
+  }
+});
